89353e4b7956d78224f5ac97820b4117153c1e4a
Mariana0317 <mariana.granara@gmail.com>
2021-09-18 18:42:52 -0300

    signin signup

---
+++ src/actions/auth.js
@@ -0,0 +1,28 @@
+import { AUTH } from "../constants/actionsTypes";
+import * as api from "../api/index.js";
+
+export const signin = (formData, history) => async (dispatch) => {
+  try {
+    //login in the user ...
+    const { data } = await api.signIn(formData);
+
+    dispatch({ type: AUTH, data});
+
+    history.push("/");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const signup = (formData, history) => async (dispatch) => {
+  try {
+    //sign up the user ...
+    const { data } = await api.signUp(formData);
+
+    dispatch({ type: AUTH, data});
+
+    history.push("/");
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -14,20 +14,42 @@ import LockOutLinedIcon from "@material-ui/icons/LockOutlined";
 import useStyles from "./styles";
 import Input from "./Input";
 import Icon from "./Icon";
+import { signin, signup } from "../../actions/auth";
+
+// inicializa los campos del formulario de registro
+
+const initialState = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
 
 const Auth = () => {
   const classes = useStyles();
   const [showPassword, setShowPassword] = useState(false);
   const [isSignup, setIsSignup] = useState(false);
+  const [formData, setFormData] = useState(initialState);
   const dispatch = useDispatch();
   const history = useHistory();
 
   const handleShowPassword = () =>
     setShowPassword((prevShowPassword) => !prevShowPassword);
 
-  const handleSubmit = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-  const handleChange = () => {};
+    if (isSignup) {
+      dispatch(signup(formData, history));
+    } else {
+      dispatch(signin(formData, history));
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
   const switchMode = () => {
     setIsSignup((prevIsSignup) => !prevIsSignup);
@@ -41,7 +63,7 @@ const Auth = () => {
     try {
       dispatch({ type: "AUTH", data: { result, token } });
 
-      history.push('/');
+      history.push("/");
     } catch (error) {
       console.log(error);
     }
@@ -70,8 +92,8 @@ const Auth = () => {
                   half
                 />
                 <Input
-                  name="firstName"
-                  label="First Name"
+                  name="lastName"
+                  label="Last Name"
                   handleChange={handleChange}
                   half
                 />
